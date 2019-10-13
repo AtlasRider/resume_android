ab6b18274de50df6b39da865330eb35edab6ddb2
Atlas Rider <atlasrider@gmail.com>
2019-10-13 13:13:34 -0700

    Small reorg, removing tabs for now. Adding resume fragment to organize content into

--- android/app/src/main/java/com/atlasrider/billdwyer/MainApplication.java
+++ android/app/src/main/java/com/atlasrider/billdwyer/MainApplication.java
@@ -3,8 +3,9 @@ package com.atlasrider.billdwyer;
 import android.app.Application;
 import android.util.Log;
 
+@SuppressWarnings("WeakerAccess")
 public class MainApplication extends Application {
-    private static String TAG = MainApplication.class.getSimpleName();
+    private static final String TAG = MainApplication.class.getSimpleName();
 
     @Override
     public void onCreate() {
---
+++ android/app/src/main/java/com/atlasrider/billdwyer/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.atlasrider.billdwyer.activity;
+
+import android.os.Bundle;
+
+import com.atlasrider.billdwyer.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
